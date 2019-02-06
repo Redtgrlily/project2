15da946b1813c5a1d00971efcb333f5832f1d7d0
redtgrlily <redtgrlily@outlook.com>
2019-02-05 18:28:04 -0600

    finished section

--- skeleton/public/assets/js/view.js
+++ skeleton/public/assets/js/view.js
@@ -44,5 +44,77 @@ $(document).ready(function() {
         }).then(getJokes);
     }
 
+    //SHowing input box for jokes to be edited
+    function editJoke() {
+        var currentJoke = $(this).data("jokeitem");
+        $(this).children().hide();
+        $(this).children("input.edit").val(currentJoke.text);
+        $(this).children("input.edit").show();
+        $(this).children("input.edit").focus();
+    }
+
+    //finishes the updates
+    function finishEdit(event) {
+        var updatedJokeList = $(this).data("jokeitem");
+        if (event.which === 13) {
+          updatedJokeList.text = $(this).children("input").val().trim();
+          $(this).blur();
+          updateJokeList(updatedJokeList);
+        }
+      }
+
+      function updateJokeList(jokeList) {
+          $.ajax({
+              method: "PUT",
+              url: "/api/jokelist",
+              data: jokeList
+          }).then(jokeList);
+      }
+
+      function cancelEdit() {
+          var currentJoke = $(this).data("jokeitem");
+          if (currentJoke) {
+            $(this).children().hide();
+            $(this).children("input.edit").val(currentJoke.text);
+            $(this).children("span").show();
+            $(this).children("button").show();
+          }
+      }
+
+      //new joke
+      function createNewRow(jokeList) {
+        var $newInputRow = $(
+          [
+            "<li class='list-group-item joke-item'>",
+            "<span>",
+            jokeList.text,
+            "</span>",
+            "<input type='text' class='edit' style='display: none;'>",
+            "<button class='delete btn btn-danger'>x</button>",
+            "<button class='complete btn btn-primary'>✓</button>",
+            "</li>"
+          ].join("")
+        );
+    
+        $newInputRow.find("button.delete").data("id", jokeList.id);
+        $newInputRow.find("input.edit").css("display", "none");
+        $newInputRow.data("jokeList", jokeList);
+        if (jokeList.complete) {
+          $newInputRow.find("span").css("text-decoration", "line-through");
+        }
+        return $newInputRow;
+      }
+      //puts joke in DB
+      function insertJoke(event) {
+        event.preventDefault();
+        var jokelist = {
+          name: $newItemInput.val().trim(),
+          text: $newItemInput.val().trim(),
+        };
+    
+        $.post("/api/jokeList", jokelist, getJokeList);
+        $newItemInput.val("");
+      }
+
     //CHARLIE START HERE!
 })
